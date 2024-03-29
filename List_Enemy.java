2d25145ab00cfcba8cac653504f5d2a4b96675a1
66070054 <66070054@kmitl.ac.th>
2024-03-29 19:58:02 +0700

    Enemy Wave 1-10

--- List_Enemy.java
+++ List_Enemy.java
@@ -14,10 +14,10 @@ public class List_Enemy {
             public void run() {
                 while (database.getStart()) {
                     if (round <= 5) {
-                        Wave_01();
+                        Wave_11();
                     }
                     else if (round <= 10) {
-                        Wave_02();
+                        Wave_01();
                     }
                     else if (round > 10) {
                         Random random = new Random();
@@ -121,6 +121,273 @@ public class List_Enemy {
         }
         round++;
     }
+
+    public void Wave_04() {
+        Random random = new Random();
+        int y;
+    
+        for (int i = 0; i < 3; i++) {
+            switch (i) {
+                case 0:
+                    Enemy_01 enemy_01 = new Enemy_01();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)database.getEnemy_01_Image().getHeight(null));
+                    enemy_01.changeLocation((int)database.getScreen().getWidth() + i*(int)database.getEnemy_01_Image().getHeight(null) + 50, y);
+                    enemis.add(enemy_01);
+                    break;
+                case 1:
+                    Enemy_02 enemy_02 = new Enemy_02();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)enemy_02.getHeight());
+                    enemy_02.changeLocation((int)database.getScreen().getWidth() + i*(int)database.getEnemy_02_Image().getHeight(null) + 50 , y);
+                    enemis.add(enemy_02);
+                    break;
+                case 2:
+                    // Enemy_03 enemy_03 = new Enemy_03();
+                    // y = random.nextInt((int)database.getScreen().getHeight() - (int)enemy_03.getHeight());
+                    // enemy_03.changeLocation((int)database.getScreen().getWidth() + i*(int)database.getEnemy_02_Image().getHeight(null) + 50 , y);
+                    // enemis.add(enemy_03);
+                    // break;
+                    Enemy_03 enemy_03 = new Enemy_03();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)enemy_03.getHeight());
+                    enemy_03.changeLocation((int)database.getScreen().getWidth(), y);
+                    enemis.add(enemy_03);
+                    break;
+            }
+        }
+        round++;
+    }
+
+    public void Wave_05() {
+        Random random = new Random();
+        int y;
+    
+        for (int i = 0; i < 3; i++) {
+            switch (i) {
+                case 0:
+                    Enemy_01 enemy_01 = new Enemy_01();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)database.getEnemy_01_Image().getHeight(null));
+                    enemy_01.changeLocation((int)database.getScreen().getWidth() + i*(int)database.getEnemy_01_Image().getHeight(null) + 50, y);
+                    enemis.add(enemy_01);
+                    break;
+                case 1:
+                    Enemy_02 enemy_02 = new Enemy_02();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)enemy_02.getHeight());
+                    enemy_02.changeLocation((int)database.getScreen().getWidth() + i*(int)database.getEnemy_02_Image().getHeight(null) + 50 , y);
+                    enemis.add(enemy_02);
+                    break;
+                case 2:
+                    Enemy_05 enemy_05 = new Enemy_05();
+                    enemy_05.changeLocation((int)database.getScreen().getWidth(), 0);
+                    enemis.add(enemy_05);
+                    break;
+            }
+        }
+        round++;
+    }
+
+    public void Wave_06() {
+        Random random = new Random();
+        int y;
+    
+        for (int i = 0; i < 3; i++) {
+            switch (i) {
+                case 0:
+                    Enemy_01 enemy_01 = new Enemy_01();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)database.getEnemy_01_Image().getHeight(null));
+                    enemy_01.changeLocation((int)database.getScreen().getWidth() + i*(int)database.getEnemy_01_Image().getHeight(null) + 50, y);
+                    enemis.add(enemy_01);
+                    break;
+                case 1:
+                    Enemy_03 enemy_03 = new Enemy_03();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)enemy_03.getHeight());
+                    enemy_03.changeLocation((int)database.getScreen().getWidth(), y);
+                    enemis.add(enemy_03);
+                    break;
+                case 2:
+                    Enemy_05 enemy_05 = new Enemy_05();
+                    enemy_05.changeLocation((int)database.getScreen().getWidth(), 0);
+                    enemis.add(enemy_05);
+                    break;
+            }
+        }
+        round++;
+    }
+
+    public void Wave_07() {
+        Random random = new Random();
+        int y;
+    
+        for (int i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    Enemy_01 enemy_01 = new Enemy_01();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)database.getEnemy_01_Image().getHeight(null));
+                    enemy_01.changeLocation((int)database.getScreen().getWidth() + i*(int)database.getEnemy_01_Image().getHeight(null) + 50, y);
+                    enemis.add(enemy_01);
+                    break;
+                case 1:
+                    Enemy_02 enemy_02 = new Enemy_02();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)enemy_02.getHeight());
+                    enemy_02.changeLocation((int)database.getScreen().getWidth() + i*(int)database.getEnemy_02_Image().getHeight(null) + 50 , y);
+                    enemis.add(enemy_02);
+                    break;
+                case 2:
+                    Enemy_03 enemy_03 = new Enemy_03();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)enemy_03.getHeight());
+                    enemy_03.changeLocation((int)database.getScreen().getWidth(), y);
+                    enemis.add(enemy_03);
+                    break;
+                case 3:
+                    Enemy_05 enemy_05 = new Enemy_05();
+                    enemy_05.changeLocation((int)database.getScreen().getWidth(), 0);
+                    enemis.add(enemy_05);
+                    break;
+            }
+        }
+        round++;
+    }
+
+    public void Wave_08() {
+        Random random = new Random();
+        int y;
+    
+        for (int i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    Enemy_02 enemy_02_1 = new Enemy_02();
+                    y = random.nextInt((int) database.getScreen().getHeight() - (int) enemy_02_1.getHeight());
+                    enemy_02_1.changeLocation((int) database.getScreen().getWidth() + i * (int) database.getEnemy_02_Image().getHeight(null) + 50, y);
+                    enemis.add(enemy_02_1);
+                    break;
+                case 1:
+                    Enemy_02 enemy_02_2 = new Enemy_02();
+                    y = random.nextInt((int) database.getScreen().getHeight() - (int) enemy_02_2.getHeight());
+                    enemy_02_2.changeLocation((int) database.getScreen().getWidth() + i * (int) database.getEnemy_02_Image().getHeight(null) + 50, y);
+                    enemis.add(enemy_02_2);
+                    break;
+                case 2:
+                    Enemy_03 enemy_03 = new Enemy_03();
+                    y = random.nextInt((int) database.getScreen().getHeight() - (int) enemy_03.getHeight());
+                    enemy_03.changeLocation((int) database.getScreen().getWidth(), y);
+                    enemis.add(enemy_03);
+                    break;
+                case 3:
+                    Enemy_05 enemy_05 = new Enemy_05();
+                    enemy_05.changeLocation((int) database.getScreen().getWidth(), 0);
+                    enemis.add(enemy_05);
+                    break;
+            }
+        }
+        round++;
+    }
+
+    public void Wave_09() {
+        Random random = new Random();
+        int y;
+    
+        for (int i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    Enemy_01 enemy_01 = new Enemy_01();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)database.getEnemy_01_Image().getHeight(null));
+                    enemy_01.changeLocation((int)database.getScreen().getWidth() + i*(int)database.getEnemy_01_Image().getHeight(null) + 50, y);
+                    enemis.add(enemy_01);
+                    break;
+                case 1:
+                    Enemy_02 enemy_02 = new Enemy_02();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)enemy_02.getHeight());
+                    enemy_02.changeLocation((int)database.getScreen().getWidth() + i*(int)database.getEnemy_02_Image().getHeight(null) + 50 , y);
+                    enemis.add(enemy_02);
+                    break;
+                case 2:
+                    Enemy_03 enemy_03 = new Enemy_03();
+                    y = random.nextInt((int) database.getScreen().getHeight() - (int) enemy_03.getHeight());
+                    enemy_03.changeLocation((int) database.getScreen().getWidth(), y);
+                    enemis.add(enemy_03);
+                    break;
+                case 3:
+                    Enemy_04 enemy_04 = new Enemy_04();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)enemy_04.getHeight());
+                    enemy_04.changeLocation((int)database.getScreen().getWidth(), y);
+                    enemis.add(enemy_04);
+                    break;
+            }
+        }
+        round++;
+    }
+
+    public void Wave_10() {
+        Random random = new Random();
+        int y;
+    
+        for (int i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    Enemy_01 enemy_01 = new Enemy_01();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)database.getEnemy_01_Image().getHeight(null));
+                    enemy_01.changeLocation((int)database.getScreen().getWidth() + i*(int)database.getEnemy_01_Image().getHeight(null) + 50, y);
+                    enemis.add(enemy_01);
+                    break;
+                case 1:
+                    Enemy_03 enemy_03 = new Enemy_03();
+                    y = random.nextInt((int) database.getScreen().getHeight() - (int) enemy_03.getHeight());
+                    enemy_03.changeLocation((int) database.getScreen().getWidth(), y);
+                    enemis.add(enemy_03);
+                    break;
+                case 2:
+                    Enemy_04 enemy_04_1 = new Enemy_04();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)enemy_04_1.getHeight());
+                    enemy_04_1.changeLocation((int)database.getScreen().getWidth(), y);
+                    enemis.add(enemy_04_1);
+                    break;
+                case 3:
+                    Enemy_04 enemy_04_2 = new Enemy_04();
+                    y = random.nextInt((int)database.getScreen().getHeight() - (int)enemy_04_2.getHeight());
+                    enemy_04_2.changeLocation((int)database.getScreen().getWidth(), y);
+                    enemis.add(enemy_04_2);
+                    break;
+            }
+        }
+        round++;
+    }
+
+    public void Wave_11() {
+        Random random = new Random();
+        int y;
+        int delay = 2500;
+    
+        for (int i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    Enemy_03 enemy_03 = new Enemy_03();
+                    y = random.nextInt((int) database.getScreen().getHeight() - (int) enemy_03.getHeight());
+                    enemy_03.changeLocation((int) database.getScreen().getWidth(), y);
+                    enemis.add(enemy_03);
+                    break;
+                case 1:
+                    Enemy_04 enemy_04_1 = new Enemy_04();
+                    y = random.nextInt((int) database.getScreen().getHeight() - (int) enemy_04_1.getHeight());
+                    enemy_04_1.changeLocation((int) database.getScreen().getWidth(), y);
+                    enemis.add(enemy_04_1);
+                    break;
+                case 2:
+                    Enemy_05 enemy_05_1 = new Enemy_05();
+                    enemy_05_1.changeLocation((int) database.getScreen().getWidth(), 0);
+                    enemis.add(enemy_05_1);
+                    break;
+                case 3:
+                    try {
+                        Thread.sleep(delay);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Enemy_05 enemy_05_2 = new Enemy_05();
+                    enemy_05_2.changeLocation((int) database.getScreen().getWidth(), 0);
+                    enemis.add(enemy_05_2);
+                    break;
+            }
+        }
+        round++;
+    }    
 
     public void draw(Graphics2D g2D, Color color) {
         for (int i = 0; i < enemis.size(); i++) {
