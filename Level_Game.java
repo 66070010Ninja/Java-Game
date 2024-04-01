4cee2170383934a639f8c2da685b9a40051ee7aa
66070019Kulkitikorn <139116732+66070019Kulkitikorn@users.noreply.github.com>
2024-04-02 02:17:33 +0700

    Delete Level_Game.java

--- Level_Game.java
+++
@@ -1,9 +0,0 @@
-public class Level_Game <T> {
-    private T level_game;
-    public void setlevel_game(T level_game) {
-        this.level_game = level_game;
-    }
-    public T getlevel_game() {
-        return level_game;
-    }
-}
